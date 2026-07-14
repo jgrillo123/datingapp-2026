6b935c34f3fc1b47722aedb5e5a5562d858dbac9
Jory <jgrillo2552@gmail.com>
2026-07-14 10:08:14 -0600

    Add and delete photos

---
+++ API/Services/PhotoService.cs
@@ -0,0 +1,47 @@
+using API.Helpers;
+using API.Interfaces;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.CodeAnalysis;
+using Microsoft.Extensions.Options;
+
+namespace API.Services;
+
+public class PhotoService : IPhotoService
+{
+    private readonly Cloudinary _cloudinary;
+
+    public PhotoService(IOptions<CloudinarySettings> config)
+    {
+        var account = new Account(config.Value.CloudName, config.Value.ApiKey, config.Value.ApiSecret);
+
+        _cloudinary = new Cloudinary(account);
+    }
+
+    public async Task<DeletionResult> DeletePhotoAsync(string publicId)
+    {
+        var deleteParms = new DeletionParams(publicId);
+
+        return await _cloudinary.DestroyAsync(deleteParms);
+    }
+
+    public async Task<ImageUploadResult> UploadPhotoAsync(IFormFile file)
+    {
+        var uploadResult = new ImageUploadResult();
+        await using var stream = file.OpenReadStream();
+
+        if (file.Length > 0)
+        {
+            var uploadParams = new ImageUploadParams
+            {
+                File = new FileDescription(file.FileName, stream),
+                Transformation = new Transformation().Height(500).Width(500).Crop("fill").Gravity("face"),
+                Folder = "da-ang20"
+            };
+
+            uploadResult = await _cloudinary.UploadAsync(uploadParams);
+        }
+
+        return uploadResult;
+    }
+}
