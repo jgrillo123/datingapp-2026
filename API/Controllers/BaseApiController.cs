0e14e0905ca95d764150c529a4f6f138e2b31863
Jory <jgrillo2552@gmail.com>
2026-07-16 10:58:17 -0600

    Paging, sorting, and filtering

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,7 +1,9 @@
+using API.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [Route("api/[controller]")]
     [ApiController]
     public class BaseApiController : ControllerBase
--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -2,6 +2,7 @@ using Api.Controllers;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -14,12 +15,14 @@ namespace API.Controllers
         IPhotoService photoService) : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
+        public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers([FromQuery]MemberParams memberParams)
         {
-            return Ok(await memberRepository.GetMembersAsync());
+            memberParams.CurrentMemberId = User.GetMemberId(); 
+
+            return Ok(await memberRepository.GetMembersAsync(memberParams));
         }
 
-        [HttpGet("{id}")] // localhost:5001/api/members/bob-id
+        [HttpGet("{id}")]
         public async Task<ActionResult<Member>> GetMember(string id)
         {
             var member = await memberRepository.GetMemberByIdAsync(id);
